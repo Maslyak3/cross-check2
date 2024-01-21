802e7980a0660154d3a895d84805241baa5b4184
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-01-21 17:07:29 +0300

    fix: change path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 
-const readStream = fs.createReadStream('./01-read-file/text.txt');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 
 readStream.on('data', (chunk) => {
   console.log(chunk.toString());
