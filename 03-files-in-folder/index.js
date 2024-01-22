2c2e8495367f9da6bdc5a0b88c44230889186c6c
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-01-22 16:44:47 +0300

    feat: complete 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToFolder, (err, fileList) => {
+  fileList.forEach((file) => {
+    const pathToFile = path.join(pathToFolder, file);
+    fs.stat(pathToFile, (err, el) => {
+      let elSize = el.size;
+      const extansion = path.extname(file);
+      if (!el.isFile()) elSize = 0;
+      console.log(
+        file.slice(0, -extansion.length),
+        '-',
+        extansion.slice(1, extansion.length),
+        '-',
+        elSize,
+      );
+    });
+  });
+});
