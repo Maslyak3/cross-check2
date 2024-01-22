5de18960a6af993c7554ed61effecf0fa62348c7
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-01-23 02:35:08 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,23 @@
 const fs = require('fs').promises;
 const path = require('path');
+const pathOriginalFolder = path.join(__dirname, 'files');
+const pathCopyFolder = path.join(__dirname, 'files-copy');
 
 async function createCopyFolder() {
-  const pathFolder = path.join(__dirname, 'files-copy');
-  await fs.mkdir(pathFolder);
+  try {
+    await fs.rm(pathCopyFolder, { recursive: true, force: true });
+  } catch (e) {
+    console.log('oops', e);
+  }
+  await fs.mkdir(pathCopyFolder, { recursive: true });
+
+  const originalFiles = await fs.readdir(pathOriginalFolder);
+  await Promise.all(
+    originalFiles.map(async (file) => {
+      const pathOriginalFolderForCopy = path.join(pathOriginalFolder, file);
+      const pathCopyFolderForCopy = path.join(pathCopyFolder, file);
+      await fs.copyFile(pathOriginalFolderForCopy, pathCopyFolderForCopy);
+    }),
+  );
 }
 createCopyFolder();
