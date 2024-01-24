7650bbcb918921060588eb7c4f4cb3bb439e5784
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-01-24 19:15:09 +0300

    feat: implement copying assets into project-dist and combine css files in one into project-dist

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,9 @@ const pathToTemplate = path.join(__dirname, 'template.html');
 const pathToComponents = path.join(__dirname, 'components');
 const pathToIndexHtml = path.join(__dirname, 'index.html');
 const pathToProjectDist = path.join(__dirname, 'project-dist');
+const pathToStyles = path.join(__dirname, 'styles');
+const pathToStyleCss = path.join(pathToProjectDist, 'style.css');
+const arrOfStyles = [];
 
 async function createSite() {
   try {
@@ -22,25 +25,70 @@ async function createSite() {
         pathToComponents,
         `${fileName}.html`, //add extansions
       );
-
       const regexp = new RegExp(`\\{\\{${fileName}\\}\\}`, 'g'); //change any {{word}} for content
       templateContent = templateContent.replace(
         regexp,
         await fs.readFile(pathToComponentsFiles, 'utf-8'),
       );
-
-      await fs.writeFile(pathToIndexHtml, templateContent, 'utf-8'); //create index.html with content
     }
-    // await fs.rm(pathToProjectDist, { recursive: true, force: true });
-    await fs.mkdir(pathToProjectDist, { recursive: true });
+
+    await fs.writeFile(pathToIndexHtml, templateContent, 'utf-8'); //create index.html with content
+
+    await fs.mkdir(pathToProjectDist, { recursive: true }); // create the project-dist
+
     // replace index.html in project-dist folder
     await fs.rename(
       pathToIndexHtml,
       path.join(pathToProjectDist, 'index.html'),
     );
+    // combine and move styles into project-dist -----------------------------------
+    const stylesFilesList = await fs.readdir(pathToStyles, { recursive: true });
+
+    await Promise.all(
+      stylesFilesList.map(async (file) => {
+        // if (path.extname(file) === '.css') {
+        const data = await fs.readFile(path.join(pathToStyles, file), 'utf-8');
+        arrOfStyles.push(data);
+        // }
+      }),
+    );
+    await fs.rm(pathToStyleCss, { recursive: true, force: true });
+    await fs.writeFile(pathToStyleCss, '');
+    await Promise.all(
+      arrOfStyles.map(async (cssData) => {
+        await fs.appendFile(pathToStyleCss, cssData);
+      }),
+    );
+    copyAssets(pathToAssets, pathToTargetAssets); // invoking function for copying assets
+    //-------------------------------------------------------------------------------
   } catch (err) {
     console.error(err);
   }
 }
 
+// copy assets into project-dist ----------------------------------------------
+const pathToAssets = path.join(__dirname, 'assets');
+const pathToTargetAssets = path.join(pathToProjectDist, 'assets');
+
+async function copyAssets(assets, target) {
+  try {
+    const assetsFilesArray = await fs.readdir(assets);
+    await fs.mkdir(target, { recursive: true });
+    await Promise.all(
+      assetsFilesArray.map(async (file) => {
+        const pathToAssets = path.join(assets, file);
+        const pathToTarget = path.join(target, file);
+        const assetsStat = await fs.stat(pathToAssets);
+        if (assetsStat.isDirectory()) {
+          await copyAssets(pathToAssets, pathToTarget);
+        } else {
+          await fs.copyFile(pathToAssets, pathToTarget);
+        }
+      }),
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+//-------------------------------------------------------------------------------
 createSite();
